ff4b9c4d916048611e1eb88a6658262da531725d
Aleksey <aliaksei.balabushka@gmail.com>
2022-12-22 09:44:32 +0300

    fix: update favicon url in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -43,7 +43,7 @@ module.exports = (options) => {
       new HtmlWebpackPlugin({
         template: path.resolve(__dirname, './src/index.html'),
         filename: 'index.html',
-        favicon: path.resolve(__dirname, './src/assets/img/favicon.svg'),
+        favicon: path.resolve(__dirname, './src/assets/icons/favicon.svg'),
         inject: 'body'
       }),
     ],

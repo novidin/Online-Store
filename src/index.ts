c09bb4edf5f666cf7187ac4ecc0ffc53d4579c38
Aleksey <aliaksei.balabushka@gmail.com>
2022-12-23 14:49:45 +0300

    feat: add page not found layout

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,31 @@ import './styles/main.scss';
 
 const app = new App();
 app.start();
+
+
+/* ===================================================== */
+
+window.onload = ():void => {
+  const addressesDOM = document.getElementById('addresses') as HTMLElement;
+  const mainDOM = document.querySelector('.main') as HTMLElement;
+
+  const renderNotFoundPage = ():void => {
+    addressesDOM.addEventListener('click', ():void => {
+      deleteChildsInMain();
+
+      const notFoundDOM = document.createElement('div') as HTMLElement;
+      notFoundDOM.classList.add('not-found');
+      notFoundDOM.innerHTML = `<div class="wrapper"><span class="not-found__image"></span><p class="not-found__title">Ой! Что-то пошло не так...</p></div>`;
+
+      mainDOM.append(notFoundDOM);
+    });
+  }
+
+  const deleteChildsInMain = ():void => {
+    while (mainDOM.firstChild) {
+      mainDOM.removeChild(mainDOM.firstChild);
+    }
+  };
+
+  renderNotFoundPage();
+}

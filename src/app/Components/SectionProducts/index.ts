3bea70ffbb5342287f0de9cee8f79ee873ea58cb
Aleksey <aliaksei.balabushka@gmail.com>
2023-01-08 00:14:53 +0300

    feat: update product page
    - product rating color is change
    - product miniatures change main image
    - add button "buy" in block
    - update adaptive

--- src/app/Components/SectionProducts/index.ts
+++ src/app/Components/SectionProducts/index.ts
@@ -81,95 +81,121 @@ class SectionProducts {
   }
 
   getProductCardHTML(product: IProduct): HTMLElement {
-    const wrapper = document.createElement('article');
-    wrapper.className = 'product product--tile-view'; // list
+    const wrapper = document.createElement('article') as HTMLElement;
+    wrapper.className = 'product product--tile-view';
+
+    let ratingStyleColor;
+
+    if (parseFloat(product.rating.overageRating) >= 4.8) {
+      ratingStyleColor = 'rating--good';
+    } else if (parseFloat(product.rating.overageRating) > 3.9) {
+      ratingStyleColor = 'rating--medium';
+    } else {
+      ratingStyleColor = 'rating--bad';
+    }
+
     wrapper.innerHTML = `
       <div class="product__column product__column--main">
-      <a href="/product?id=${product.id}" data-local-link id="productTitle" class="product__title">
-        <h3 class="product__brand">${product.brand}</h3>
-        <span class="product__model">${product.model}</span>
-        <span class="product__sizes">${product.size}</span>
-      </a>
+       <h3 class="product__header">
+          <a id="productTitle" class="product__title" href="/product?id=${product.id}" data-local-link>
+            <span class="product__brand">${product.brand}</span>
+            <span class="product__model">${product.model}</span>
+            <span class="product__sizes">${product.size}</span>
+          </a>
+        </h3>
+      <ul class="product__miniatures-list">
+          <li class="product__miniatures-item product__miniatures-item--active">
+            <img class="product__miniatures-image"
+                 src="${product.imageUrl[0]}" alt="${product.brand} ${product.model}">
+          </li>
+          <li class="product__miniatures-item">
+            <img class="product__miniatures-image"
+                 src="${product.imageUrl[1]}" alt="${product.brand} ${product.model}">
+          </li>
+        </ul>
       <img class="product__image"
           src="${product.imageUrl[0]}"
           alt="${product.brand} ${product.model}">
     </div>
-    <div class="product__column product__column--short">
-      <div class="product__subtitle product__subtitle--top">
-        <span class="product__price">${product.price} руб</span>
-        <span class="product__count">В наличии: ${product.count} шт.</span>
-      </div>
-      <ul class="product__labels-list">
-        <li class="product__labels-item">
-          <span class="product__season icon icon--${product.season}"></span>
-          <span class="product__labels-title">${seasonNames[product.season]}</span>
-        </li>
-        <li class="product__labels-item">
-          <p class="product__rating">
-            <span class="icon icon--star rating rating--good"></span>
-            <span class="rating__value rating--good">${product.rating.overageRating}</span>
-          </p>
-          <span class="product__labels-title">Рейтинг</span>
-        </li>
-        <li class="product__labels-item">
-          <span class="product__comments">${product.rating.commentsCount}</span>
-          <span class="product__labels-title">Комент</span>
-        </li>
-      </ul>
-      <ul class="product__features-list">
-        <li class="product__features-item">
-          <span class="product__features-title">Глубина протектора</span>
-          <span class="product__features-value">${product.features.protector} мм</span>
-        </li>
-        <li class="product__features-item">
-          <span class="product__features-title">Вес</span>
-          <span class="product__features-value">${product.features.weight} кг</span>
-        </li>
-        <li class="product__features-item">
-          <span class="product__features-title">Эксплуатация</span>
-          <span class="product__features-value">${product.features.exploitation}</span>
-        </li>
-        <li class="product__features-item">
-          <span class="product__features-title">Защита диска</span>
-          <span class="product__features-value">${product.features.discProtector}</span>
-        </li>
-        <li class="product__features-item">
-          <span class="product__features-title">Индекс нагрузки</span>
-          <span class="product__features-value">${product.features.loadIndex}</span>
-        </li>
-        <li class="product__features-item">
-          <span class="product__features-title">Индекс скорости</span>
-          <span class="product__features-value">${product.features.speedIndex}</span>
-        </li>
-      </ul>
-    </div>
-    <div class="product__column product__column--price">
-      <div class="product__subtitle">
-        <p class="product__price">${product.price} руб</p>
-        <p class="product__count">В наличии: ${product.count} шт.</p>
-      </div>
-      <ul class="product__offers-list">
-          <li class="product__offers-item">
-            <span class="product__offers-icon icon icon--pay"></span>
-            <p class="product__offers-title">Оплата частями</p>
+      <div class="product__column product__column--short">
+        <div class="product__subtitle product__subtitle--top">
+          <span class="product__price">${product.price} руб</span>
+          <span class="product__count">В наличии: ${product.count} шт.</span>
+        </div>
+        <ul class="product__labels-list">
+          <li class="product__labels-item">
+            <span class="product__season icon icon--${product.season}"></span>
+            <span class="product__labels-title">${seasonNames[product.season]}</span>
           </li>
-          <li class="product__offers-item">
-            <span class="product__offers-icon icon icon--guarantee"></span>
-            <p class="product__offers-title">Официальная гарантия</p>
+          <li class="product__labels-item">
+            <p class="product__rating">
+              <span class="icon icon--star rating ${ratingStyleColor}"></span>
+              <span class="rating__value ${ratingStyleColor}">${product.rating.overageRating}</span>
+            </p>
+            <span class="product__labels-title">Рейтинг</span>
           </li>
-          <li class="product__offers-item">
-            <span class="product__offers-icon icon icon--shipping"></span>
-            <p class="product__offers-title">Доставка по РБ</p>
+          <li class="product__labels-item">
+            <span class="product__comments">${product.rating.commentsCount}</span>
+            <span class="product__labels-title">Комент</span>
           </li>
         </ul>
-      <button class="product__button button">В корзину</button>
-    </div>
+        <ul class="product__features-list">
+          <li class="product__features-item">
+            <span class="product__features-title">Глубина протектора</span>
+            <span class="product__features-value">${product.features.protector} мм</span>
+          </li>
+          <li class="product__features-item">
+            <span class="product__features-title">Вес</span>
+            <span class="product__features-value">${product.features.weight} кг</span>
+          </li>
+          <li class="product__features-item">
+            <span class="product__features-title">Эксплуатация</span>
+            <span class="product__features-value">${product.features.exploitation}</span>
+          </li>
+          <li class="product__features-item">
+            <span class="product__features-title">Защита диска</span>
+            <span class="product__features-value">${product.features.discProtector}</span>
+          </li>
+          <li class="product__features-item">
+            <span class="product__features-title">Индекс нагрузки</span>
+            <span class="product__features-value">${product.features.loadIndex}</span>
+          </li>
+          <li class="product__features-item">
+            <span class="product__features-title">Индекс скорости</span>
+            <span class="product__features-value">${product.features.speedIndex}</span>
+          </li>
+        </ul>
+      </div>
+      <div class="product__column product__column--price">
+        <div class="product__subtitle">
+          <p class="product__price">${product.price} руб</p>
+          <p class="product__count">В наличии: ${product.count} шт.</p>
+        </div>
+        <ul class="product__offers-list">
+            <li class="product__offers-item">
+              <span class="product__offers-icon icon icon--pay"></span>
+              <p class="product__offers-title">Оплата частями</p>
+            </li>
+            <li class="product__offers-item">
+              <span class="product__offers-icon icon icon--guarantee"></span>
+              <p class="product__offers-title">Официальная гарантия</p>
+            </li>
+            <li class="product__offers-item">
+              <span class="product__offers-icon icon icon--shipping"></span>
+              <p class="product__offers-title">Доставка по РБ</p>
+            </li>
+          </ul>
+        <button class="product__button button">В корзину</button>
+      </div>
     `;
+
     const cartButton = wrapper.querySelector('.button') as HTMLButtonElement;
+
     if (cartStorage.isProductInCart(product.id)) {
       cartButton.textContent = 'В корзине';
-      cartButton.style.backgroundColor = '#747b8f';
+      cartButton.classList.add('button--accent');
     }
+
     cartButton.onclick = () => {
       if (cartStorage.isProductInCart(product.id)) {
         cartStorage.removeProduct(product.id);

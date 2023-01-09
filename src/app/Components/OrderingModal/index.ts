a745a97cd6609e522a9565c00d8402ae6d614def
Aleksey <aliaksei.balabushka@gmail.com>
2023-01-09 14:54:41 +0300

    feat: add ordering modal with validation

---
+++ src/app/Components/OrderingModal/index.ts
@@ -0,0 +1,224 @@
+import imageCard from '../../../assets/icons/order/card.svg';
+import imageVisa from '../../../assets/icons/order/visa.svg';
+import imageMastercard from '../../../assets/icons/order/mastercard.svg';
+import imageBelcard from '../../../assets/icons/order/belcard.svg';
+
+class OrderingModal {
+
+  private readonly mainDOM: HTMLElement;
+
+  private readonly modalDOM: HTMLElement;
+
+  private readonly shadowDOM: HTMLElement;
+
+  constructor() {
+    this.mainDOM = document.querySelector('main') as HTMLElement;
+    this.modalDOM = this.createModal();
+    this.shadowDOM = this.createShadow();
+  }
+
+  createModal(): HTMLElement {
+    const wrapper = document.createElement('div');
+    wrapper.classList.add('ordering');
+    return wrapper;
+  }
+
+  createShadow(): HTMLElement {
+    const shadow = document.createElement('div');
+    shadow.classList.add('shadow', 'shadow--active');
+    return shadow;
+  }
+
+  start() {
+    this.render();
+    this.openModal();
+    this.validate();
+  }
+
+  render() {
+    this.modalDOM.append(createContent());
+
+    function createContent(): HTMLDivElement {
+      const containerDOM = document.createElement('div');
+      containerDOM.classList.add('ordering__container');
+      containerDOM.innerHTML = `<h2 class="ordering__header">Оформление заказа</h2>`;
+
+      containerDOM.append(createForm());
+      return containerDOM;
+    }
+
+    function createForm(): HTMLFormElement {
+      const formDOM = document.createElement('form');
+      formDOM.classList.add('ordering__form');
+      formDOM.innerHTML = `
+        <div class="ordering__form-column">
+          <div class="ordering__form-line">
+            <label class="ordering__form-label" for="orderingName">Имя и Фамилия</label>
+            <input class="ordering__form-input" type="text" name="orderingName" id="orderingName" autocomplete="off" required>
+            <span class="ordering__form-error">* Неверное значение</span>
+          </div>
+          <div class="ordering__form-line">
+            <label class="ordering__form-label" for="orderingPhone">Номер телефона</label>
+            <input class="ordering__form-input" type="text" name="orderingPhone" id="orderingPhone" autocomplete="off" required>
+            <span class="ordering__form-error">* Неверное значение</span>
+          </div>
+          <div class="ordering__form-line">
+            <label class="ordering__form-label" for="orderingAddress">Адрес доставки</label>
+            <input class="ordering__form-input" type="text" name="orderingAddress" id="orderingAddress" autocomplete="off" required>
+            <span class="ordering__form-error">* Неверное значение</span>
+          </div>
+          <div class="ordering__form-line">
+            <label class="ordering__form-label" for="orderingEmail">Почта</label>
+            <input class="ordering__form-input" type="text" name="orderingEmail" id="orderingEmail" autocomplete="off" required>
+            <span class="ordering__form-error">* Неверное значение</span>
+          </div>
+        </div>
+        <div class="ordering__form-column">
+          <div class="ordering__form-line">
+            <label class="ordering__form-label" for="orderingCardNumber">Номер карты</label>
+            <div class="ordering__form-card-number">
+              <input class="ordering__form-input" type="text" name="orderingCardNumber" id="orderingCardNumber"
+                     autocomplete="off" required>
+              <span class="ordering__form-error">* Неверное значение</span>
+              <div class="ordering__form-image">
+                <img src=${imageCard} alt="Неопределено">
+              </div>
+            </div>
+          </div>
+          <div class="ordering__form-line">
+            <label class="ordering__form-label" for="orderingExpirationDate">Срок действия</label>
+            <input class="ordering__form-input" type="text" name="orderingExpirationDate" id="orderingExpirationDate"
+                   autocomplete="off" required>
+            <span class="ordering__form-error">* Неверное значение</span>
+          </div>
+          <div class="ordering__form-line">
+            <label class="ordering__form-label" for="orderingCVV">CVV</label>
+            <input class="ordering__form-input" type="text" name="orderingCVV" id="orderingCVV" autocomplete="off" required>
+            <span class="ordering__form-error">* Неверное значение</span>
+          </div>
+          <div class="ordering__form-line">
+            <label class="ordering__form-label ordering__form-label--hidden">Подтвердить</label>
+            <button class="button ordering__button">Заказать</button>
+          </div>
+        </div>
+      `;
+
+      return formDOM;
+    }
+  }
+
+  openModal() {
+    this.mainDOM.append(this.modalDOM, this.shadowDOM);
+    this.shadowDOM.addEventListener('click', (): void => this.closeModal());
+  }
+
+  closeModal() {
+    this.modalDOM.remove();
+    this.shadowDOM.remove();
+  }
+
+  validate() {
+    const formDOM = document.querySelector('.ordering__form') as HTMLFormElement;
+    const buttonDOM = document.querySelector('.ordering__button') as HTMLButtonElement;
+
+    const inputErrors = {
+      orderingName: false,
+      orderingPhone: false,
+      orderingAddress: false,
+      orderingEmail: false,
+      orderingCardNumber: false,
+      orderingExpirationDate: false,
+      orderingCVV: false
+    };
+
+    formDOM.addEventListener('input', (e: Event): void => {
+      const target = e.target as HTMLFormElement;
+      const errorDOM = target.nextElementSibling as HTMLElement;
+      const validateResult = checkValue(target.value, target.id);
+
+      if (target.id === 'orderingExpirationDate') watchExpirationDate();
+
+      if (target.id === 'orderingCardNumber') updateCardImage();
+
+      updateErrorStyle();
+
+      function checkValue(value: string, id: string): boolean {
+
+        const patterns = {
+          orderingName: /^[a-zA-Zа-яА-Я]{3,12}[' '][a-zA-Zа-яА-Я]{3,12}$/,
+          orderingPhone: /[+][0-9]{9,12}$/,
+          orderingAddress: /^[a-zA-Zа-яА-Я]{5,30}[' '][a-zA-Zа-яА-Я]{5,30}[' '][a-zA-Zа-яА-Я]{5,30}$/,
+          orderingEmail: /^([a-z0-9]{6,12})+@([a-z]+\.)+[a-z]{2,4}$/,
+          orderingCardNumber: /^[0-9]{16}$/,
+          orderingExpirationDate: /^(0[1-9]|1[0-2])\/([0-9]{2}|[0-9]{2})$/,
+          orderingCVV: /^[0-9]{3}$/
+        };
+
+        // @ts-ignore
+        const regExp = new RegExp(patterns[id]);
+        const result = regExp.test(value);
+
+        // @ts-ignore
+        if (id !== '') inputErrors[id] = result;
+
+        return result
+      }
+
+      function updateErrorStyle() {
+        validateResult
+          ? errorDOM.classList.remove('ordering__form-error--active')
+          : errorDOM.classList.add('ordering__form-error--active');
+      }
+
+      function watchExpirationDate() {
+        const symbol = '/';
+
+        if (target.value.length === 2) {
+          // @ts-ignore
+          e.target.value = e.inputType === 'deleteContentBackward'
+            ? target.value.split(symbol)[0]
+            : target.value + symbol
+        } else if (target.value.length > 4) {
+          // @ts-ignore
+          e.target.value = target.value.slice(0, 5);
+        }
+      }
+
+      function updateCardImage() {
+        const formImageDOM = document.querySelector('.ordering__form-image') as HTMLElement;
+
+        switch (target.value[0]) {
+          case '4':
+            formImageDOM.innerHTML = `<img src=${imageVisa} alt="Visa">`
+            break;
+          case '5':
+            formImageDOM.innerHTML = `<img src=${imageMastercard} alt="Mastercard">`
+            break;
+          case '9':
+            formImageDOM.innerHTML = `<img src=${imageBelcard} alt="Белкард">`
+            break;
+          default:
+            formImageDOM.innerHTML = `<img src=${imageCard} alt="Неопределено">`
+        }
+      }
+    });
+
+    buttonDOM.addEventListener('click', (e: Event): void => {
+      e.preventDefault();
+
+      const errors = Object.values(inputErrors);
+
+      if (errors.every(item => item)) {
+        console.log('validate successful');
+      } else if (errors.every(item => !item)) {
+        // If all inputs are empty and user click button
+        const errorSpanDOM = document.querySelectorAll('.ordering__form-error') as NodeListOf<HTMLElement>;
+        errorSpanDOM.forEach((item) => item.classList.add('ordering__form-error--active'));
+      } else {
+        console.log('validate error');
+      }
+    });
+  }
+}
+
+export default OrderingModal;
--- src/app/Components/SectionCartTotal/index.ts
+++ src/app/Components/SectionCartTotal/index.ts
@@ -1,55 +1,96 @@
-import cartStorage from "../../Storage/Cart";
+import cartStorage from '../../Storage/Cart';
+
 class SectionCartTotal {
 
-  private totalWrapper: HTMLElement;
-  private totalColumn: HTMLElement;
+  private readonly promoContentDOM: HTMLElement;
 
   constructor() {
-    this.totalWrapper = document.createElement('div');
-    this.totalWrapper.className = 'promo__content';
-
-    this.totalColumn = document.createElement('div');
-    this.totalColumn.className = 'promo__column';
-    this.totalWrapper.appendChild(this.totalColumn);
+    this.promoContentDOM = this.createContainer();
 
     window.addEventListener('addedToCard', () => {
-      console.log('kjgkjhkjh')
-      this.buildTotalColumnHTML();
-    })
+      this.promoContentDOM.removeChild(this.promoContentDOM.children[0]);
+      this.promoContentDOM.prepend(this.createTotalColumnDOM());
+      console.log('addedToCard Event');
+    });
+  }
+
+  createContainer(): HTMLElement {
+    const container = document.createElement('div');
+    container.classList.add('promo__content');
+    return container;
   }
 
   getHTML() {
+    this.promoContentDOM.innerHTML = '';
 
-    this.buildTotalColumnHTML();
-    return this.totalWrapper;
+    const columnsDOM = [this.createTotalColumnDOM(), this.createActivateColumnDOM(), this.createConfirmColumnDOM()];
+
+    this.promoContentDOM.append(...columnsDOM);
+    return this.promoContentDOM;
   }
 
-  buildTotalColumnHTML(promoPerc = 0) {
-    this.totalColumn.innerHTML = '';
+  createTotalColumnDOM(promoPerc = 0): HTMLDivElement {
+    const columnDOM = this.createColumnDOM() as HTMLDivElement;
+
     const cartCount = cartStorage.getCountProducts().toString();
     const cartTotal = cartStorage.getTotal().toString();
-    // const totalSum = +cartCount * +cartTotal;
     const promoSum = +cartTotal * (promoPerc / 100);
-    this.totalColumn.innerHTML = `
-    <ul class="promo__list">
-      <li class="promo__item">
-        <span class="promo__item-title">Количество</span>
-        <span class="promo__item-value">${cartCount} шт.</span>
-      </li>
-      <li class="promo__item">
-        <span class="promo__item-title">Стоимость</span>
-        <span class="promo__item-value">${cartTotal} руб</span>
-      </li>
-      <li class="promo__item">
-        <span class="promo__item-title">Скидка</span>
-        <span class="promo__item-value">- ${promoSum} руб</span>
-      </li>
-      <li class="promo__item promo__item--main">
-        <span class="promo__item-title">Итого</span>
-        <span class="promo__item-value">${+cartTotal - promoSum} руб</span>
-      </li>
-    </ul>
-    `
+
+    columnDOM.innerHTML = `
+      <ul class="promo__list">
+        <li class="promo__item">
+          <span class="promo__item-title">Количество</span>
+          <span class="promo__item-value">${cartCount} шт.</span>
+        </li>
+        <li class="promo__item">
+          <span class="promo__item-title">Стоимость</span>
+          <span class="promo__item-value">${cartTotal} руб</span>
+        </li>
+        <li class="promo__item">
+           <span class="promo__item-title">Скидка</span>
+           <span class="promo__item-value">- ${promoSum} руб</span>
+        </li>
+        <li class="promo__item promo__item--main">
+           <span class="promo__item-title">Итого</span>
+           <span class="promo__item-value">${+cartTotal - promoSum} руб</span>
+        </li>
+      </ul>
+    `;
+
+    return columnDOM;
+  };
+
+  createActivateColumnDOM(): HTMLDivElement {
+    const columnDOM = this.createColumnDOM() as HTMLDivElement;
+
+    columnDOM.innerHTML = `
+      <ul class="promo__activated-list">
+        <li class="promo__activated-item">
+          <span class="promo__activated-code">LOLOLO</span>
+          <span class="promo__activated-delete">Удалить</span>
+        </li>
+      </ul>
+      <div class="promo__activate">
+        <input class="promo__input" type="text" placeholder="Введите промокод" autocomplete="off">
+        <button class="button button--special">Применить</button>
+      </div>
+    `;
+
+    return columnDOM;
+  };
+
+  createConfirmColumnDOM(): HTMLDivElement {
+    const columnDOM = this.createColumnDOM() as HTMLDivElement;
+    columnDOM.innerHTML = `<button id="orderingButton" class="button promo__button">Оформить заказ</button>`;
+
+    return columnDOM;
+  }
+
+  createColumnDOM(): HTMLDivElement {
+    const columnDOM = document.createElement('div');
+    columnDOM.classList.add('promo__column');
+    return columnDOM;
   }
 }
+
 export default SectionCartTotal;

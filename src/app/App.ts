422ae57a1a30ed37f1a6472d3a1465a8a6cbacb4
Aleksey <aliaksei.balabushka@gmail.com>
2023-01-13 13:44:42 +0300

    refactor: in app.ts move check localstorage to start method

--- src/app/App.ts
+++ src/app/App.ts
@@ -2,25 +2,22 @@ import router from './Router/index';
 import cartStorage from './Storage/Cart';
 import { ICartProduct } from './Types';
 
-
 class App {
 
   private router;
 
   constructor() {
     this.router = router;
-
-    this.getLocalStorage();
-    window.addEventListener('beforeunload', this.setLocalStorage.bind(this));
   }
 
   start(): void {
+    this.getLocalStorage();
+    window.addEventListener('beforeunload', this.setLocalStorage.bind(this));
     this.router.start();
   }
 
   setLocalStorage(): void {
     const cartItems = cartStorage.getCartProducts();
-
     localStorage.setItem('cartItems', JSON.stringify(cartItems));
   }
 

d2f35dbcffbf6af1f28099ac44a434f6562af5f2
Aleksey <aliaksei.balabushka@gmail.com>
2023-01-16 16:17:40 +0300

    refactor: refactor buttonUrlCopy class

--- src/app/Components/ButtonCopyURL/index.ts
+++ src/app/Components/ButtonCopyURL/index.ts
@@ -2,32 +2,41 @@ import { buttonCopyURLContent } from '../../Storage/consts';
 
 class ButtonCopyURL {
 
-  private button: HTMLButtonElement;
+  private readonly button: HTMLButtonElement;
 
   constructor() {
-    this.button = document.createElement('button');
-    this.button.className = 'button search__button-copy';
-    this.button.title = 'Copy URL';
-    this.button.onclick = () => {
-      this.clickHandler();
-    }
-    this.enableButton();
+    this.button = this.createButton();
+  }
+
+  private createButton(): HTMLButtonElement {
+    const buttonDOM = document.createElement('button');
+    buttonDOM.className = 'button search__button-copy';
+    buttonDOM.title = 'Copy URL';
+
+    buttonDOM.innerHTML = buttonCopyURLContent.ready;
+    buttonDOM.disabled = false;
+
+    buttonDOM.addEventListener('click', () => this.clickHandler());
+
+    return buttonDOM;
   }
 
   private clickHandler(): void {
     const currURL: string = location.toString()
+
     navigator.clipboard.writeText(currURL).then(() => {
       this.pauseButton(buttonCopyURLContent.success);
     }).catch((err: Error) => {
       this.pauseButton(buttonCopyURLContent.error);
       alert(err);
-    })
+    });
   }
 
   private pauseButton(message: string): void {
     this.button.innerHTML = message;
     this.button.disabled = true;
     this.button.classList.add('button--disable');
+
     setTimeout(() => {
       this.button.classList.remove('button--disable');
       this.enableButton();
